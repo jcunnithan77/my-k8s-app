0f08816d0dc7312cad55a70673e9325e2acf8137
junnithan <jithincunnithan@gmail.com>
2026-04-07 16:29:14 +0300

    updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const os = require('os');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(`
-        <h1>Hello from Jithin's Ubuntu Server!</h1>
+        <h1>Hello from Jithin's  Server!</h1>
         <p><b>Status:</b> Deployment Successful via GitHub Actions</p>
         <p><b>Pod Name:</b> ${os.hostname()}</p>
         <p><b>Location:</b> Riyadh, KSA</p>
